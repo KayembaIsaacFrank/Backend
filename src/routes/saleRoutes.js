4a3b37325b6b97468c424b22853103fba90c305b
Francis Precious Wagonda <franciswagonda@gmail.com>
2025-11-14 01:34:53 +0300

    deleted:    .env 	deleted:    index.js 	deleted:    package-lock.json 	deleted:    package.json 	deleted:    src/config/config.json 	deleted:    src/config/database.js 	deleted:    src/controllers/analyticsController.js 	deleted:    src/controllers/authController.js 	deleted:    src/controllers/creditController.js 	deleted:    src/controllers/procurementController.js 	deleted:    src/controllers/productController.js 	deleted:    src/controllers/reportsController.js 	deleted:    src/controllers/saleController.js 	deleted:    src/controllers/salesController.js 	deleted:    src/controllers/stockController.js 	deleted:    src/middleware/auth.js 	deleted:    src/middleware/authMiddleware.js 	deleted:    src/middleware/authenticateToken.js 	deleted:    src/middleware/authorizeRole.js 	deleted:    src/middleware/errorHandler.js 	deleted:    src/middleware/validation.js 	deleted:    src/models/Product.js 	deleted:    src/models/Sale.js 	deleted:    src/models/SaleDetail.js 	deleted:    src/models/User.js 	deleted:    src/models/index.js 	deleted:    src/routes/analyticsRoutes.js 	deleted:    src/routes/authRoutes.js 	deleted:    src/routes/creditRoutes.js 	deleted:    src/routes/index.js 	deleted:    src/routes/procurementRoutes.js 	deleted:    src/routes/productRoutes.js 	deleted:    src/routes/reportsRoutes.js 	deleted:    src/routes/saleRoutes.js 	deleted:    src/routes/salesRoutes.js 	deleted:    src/routes/stockRoutes.js

--- src/routes/saleRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const saleController = require('../controllers/saleController');
-const authMiddleware = require('../middleware/authMiddleware');
-
-router.post('/', authMiddleware.isUser, saleController.createSale);
-
-module.exports = router;
